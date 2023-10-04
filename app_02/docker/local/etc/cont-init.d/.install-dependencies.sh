060b9241e42f58663c2e13535b43e9cb226f7e13
Vinh Vo <vohuynhvinh@gmail.com>
2023-10-04 21:04:53 +0700

    Init dependence package

--- app_02/docker/local/etc/cont-init.d/.install-dependencies.sh
+++ app_02/docker/local/etc/cont-init.d/.install-dependencies.sh
@@ -3,4 +3,4 @@
 source /root/.bashrc
 echo -e "${BLUE}--- Install composer dependencies ---${NC}"
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_MEMORY_LIMIT=-1 php /usr/bin/composer install --prefer-dist --no-interaction --optimize-autoloader --no-progress --no-suggest
+COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_MEMORY_LIMIT=-1 php /usr/local/bin/composer install --prefer-dist --no-interaction --optimize-autoloader --no-progress --no-suggest
